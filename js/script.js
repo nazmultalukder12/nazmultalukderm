1e861afd4b5de0098c008eb17d5c655a2a566cac
nazmultalukder12 <96172159+nazmultalukder12@users.noreply.github.com>
2022-02-18 03:40:11 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        items:3,
+        loop: true,
+        autoplay: true,
+    });
+  });
+
+ 
+ $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+
+ 
